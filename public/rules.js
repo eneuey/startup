3852c322fb31c9666a65f034a516a7aca9ada6a6
Ethan Neuenschwander <ethanneuen2003@gmail.com>
2023-04-12 23:25:34 -0600

    turned into service and added quote generator

---
+++ public/rules.js
@@ -0,0 +1,39 @@
+// function displayQuote(data) {
+//     fetch('https://api.quotable.io/random')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const containerEl = document.querySelector('#quote');
+  
+//         const quoteEl = document.createElement('p');
+//         quoteEl.classList.add('quote');
+//         const authorEl = document.createElement('p');
+//         authorEl.classList.add('author');
+  
+//         quoteEl.textContent = data.content;
+//         authorEl.textContent = data.author;
+  
+//         containerEl.appendChild(quoteEl);
+//         containerEl.appendChild(authorEl);
+//     });
+// }
+
+// displayQuote()
+
+
+
+function NorrisDisplay(data) {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#quote');
+  
+        const quoteEl = document.createElement('p');
+        quoteEl.classList.add('quote');
+  
+        quoteEl.textContent = data.value;
+  
+        containerEl.appendChild(quoteEl);
+    });
+}
+
+NorrisDisplay()
