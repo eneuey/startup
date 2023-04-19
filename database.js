a6c8a006a43a2b1a62e3b59b77d3b8de00bad7f4
Ethan Neuenschwander <ethanneuen2003@gmail.com>
2023-04-18 18:02:08 -0600

    Added authentication

--- database.js
+++ database.js
@@ -1,4 +1,7 @@
 const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -12,6 +15,34 @@ const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
 const scoreCollection = client.db('startup').collection('score');
+const userCollection = client.db('startup').collection('user');
+
+
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
+
+
+
 
 function addScore(score) {
     scoreCollection.insertOne(score);
@@ -47,5 +78,5 @@ function addScore(score) {
     
   }
   
-  module.exports = {addScore, getHighScores, hasPlayer, updateScore};
+  module.exports = {addScore, getHighScores, hasPlayer, updateScore, getUser, getUserByToken, createUser};
   
